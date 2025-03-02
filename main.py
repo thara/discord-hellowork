38856eecaf2773bccb4e23465efd4e8d36675af0
Tomochika Hara <git@thara.dev>
2025-03-02 19:52:20 +0900

    :mag: Verbose pycord logs

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ import env
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
 
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
 if __name__ == "__main__":
     bot.run(env.DISCORD_TOKEN)
